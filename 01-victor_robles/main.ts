7062c41eee5f1581348b73369a1e0b097ba5ebc7
Emmanuel Sierra <emmanuel.sierra>
2024-11-15 23:45:12 -0400

    curso 15min victor robles terminado

--- 01-victor_robles/main.ts
+++ 01-victor_robles/main.ts
@@ -12,5 +12,24 @@ let personas:string[] = ["Emmanuel", "Ana", "Diana", "Coco"];
 console.log(texto, numero, falso, cualquiera, nombre);
 console.log(personas);
 
+let div_personas:HTMLElement | null = document.querySelector("#personas");
+
+// se hace asi, se ve bien, pero si inspeccionamos la pagina, se ve que primero que se crea las etiquetas de apertura y cierre, luego los elementos li, porque estos se tardan mas.
+/*
+div_personas.innerHTML = "<ul>";
+personas.map((persona) => {
+    div_personas.innerHTML += `<li>${persona}</li>`;
+});
+div_personas.innerHTML += "</ul>";
+*/
+
+// es mas efectivo hacerlo asi
+div_personas.innerHTML = "<ul>" +
+                            personas.map((persona) => {
+                                return `<li>${persona}</li>`;
+                            }).join("");
+                            + "</ul>";
+
+// de esta forma, si inspeccionamos se ve que se crea correctamente
 
 
